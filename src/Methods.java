48e3155dcbce602aed11d4cb812901cdbe4113ed
Kristina Mickaela Fujiwara <98295543+kristinafujiwara@users.noreply.github.com>
2022-12-04 00:00:35 +0800

    Create Methods.java

---
+++ src/Methods.java
@@ -0,0 +1,8 @@
+class Methods {
+    public static int computeValue(String value1, String value2) {
+        int num1 = Integer.parseInt(value1);
+        int num2 = Integer.parseInt(value2);
+        int difference = num1 - num2;
+        return difference;
+    }
+}
