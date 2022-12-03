2a4db5c49dc518b101f490c41bbf5cda3f6564e8
Kristina Mickaela Fujiwara <98295543+kristinafujiwara@users.noreply.github.com>
2022-12-03 23:59:26 +0800

    Create Result.java

---
+++ src/Result.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+class Result extends Methods {
+    public static void displayMessage(String answer){
+        JOptionPane.showMessageDialog( null, "Your age is: " + answer);
+    }
+}
